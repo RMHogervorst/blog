95d99cc969806b89da99ea6ce80c97e25304f1d1
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2020-11-28 18:09:27 +0100

    optimized images

--- useful_commands.sh
+++ useful_commands.sh
@@ -1,7 +1,12 @@
 #  useful scripts for blog
 # use fd for fast finding brew install fd
 # brew install optipng
-
+#### Add this as githook?
 # compress all images with optipng
 # go to static folder and execute
-fd "\.png" -exec optipng {} \;
+find . -name "*.png" -exec optipng '{}' \;
+find . -name "*.jpg" -exec jpegoptim --all-progressive --strip-all '{}' \;
+
+# go to content/ folder and execute
+find . -name "*.png" -exec optipng '{}' \;
+find . -name "*.jpg" -exec jpegoptim --all-progressive --strip-all '{}' \;
